76282db2f99719a5f583a18d60821848615d5729
samapikasahoo <samapika.sahoo2@gmail.com>
2022-06-03 12:04:09 +0200

    Added h1 tag

---
+++ main.js
@@ -0,0 +1,21 @@
+printOut()  {
+    var abc = this.head
+    while(abc){
+        console.log(abc.item)
+        abc=abc.next
+    }
+}
+myList.printOut()
+printIndex(value){
+    var xyz = this.head
+    var indexCount = 0
+    if (xyz =null)
+    { 
+        return -1
+    }
+    else{
+       while ( xyz != null){
+           if
+       }
+    }
+}
