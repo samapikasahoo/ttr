c64e1deff1e6d04cddd40aebabc1cfbc3873e285
samapikasahoo <samapika.sahoo2@gmail.com>
2022-06-21 12:48:30 +0200

    'Fix'

---
+++ index2.js
@@ -0,0 +1,64 @@
+document .body.style.backgroundColor="blue"
+console.log(
+  documentgetElementById("myId").style
+)
+/*function abc() {
+  leta=documentgetElementByTagname("input")[0].value
+  console.log(a)
+  document.body.style.backgroundColor = a  
+}*/
+/*function abc(){
+  let myColor = document.getElementById("myInput").value
+  if(myColor.length == 0)
+  {
+  document.body.style.backgroundColor = "Green"
+  }
+  else{
+  document.body.style.backgroundColor = myColor
+  }
+}*/
+/*function abc(){
+  let myEmail = document.getElementById("myInput").value
+  let mydata = myEmail.split('@')
+  let myId = mydata[1].split('.')
+  if(myId[0] == "gmail")
+  {
+  console.log("The given Mail id is Gmail")
+  }
+  
+  
+  
+  else if(myId[0] == "yahoo"){
+  console.log("The given Mail id is Yahoo")
+  }
+  else{
+  console.log("The given Mail id is neither gamil nor yahoo")
+  // }*
+}*/
+/*function enter(){
+  document.body.style.backgroundColor = "red"
+  }
+  function leave(){
+  document.body.style.backgroundColor = "yellow"
+  }
+  function over(){
+  document.body.style.backgroundColor = "blue"
+  }
+  function enter(){
+document.body.style.backgroundColor = "red"
+}
+function leave(){
+document.body.style.backgroundColor = "yellow"
+}
+function myclick(){
+document.getElementsByTagName("p")[0]. innerHTML = "Hey I am Click"
+}
+
+
+
+function dblclick(){
+document.getElementsByTagName("p")[0]. innerHTML = "Hey I am Double Click"
+}*/
+function myChange(){
+  console.log(document.getElementById("new").value)
+  }
